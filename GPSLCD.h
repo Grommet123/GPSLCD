384d662c0ce9628c8d37d65c5d016925b714f07f
Gary Grotsky <gkgrotsky@aol.com>
2021-11-20 13:41:58 -0500

    v9.10 Change baud rate for serial

--- GPSLCD.h
+++ GPSLCD.h
@@ -28,7 +28,7 @@
 #ifndef GPSLCD_h
 #define GPSLCD_h
 
-#define VERSION  "9.9"               // Version number
+#define VERSION  "9.10"              // Version number
 //#define Debug                      // Comment out for debug
 //#define DATA_VALID_OVERRIDE        // Override the data valid flag (comment out to turn off)
 // It also feeds fake GPS data to the system for debugging
