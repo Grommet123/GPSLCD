bdf39b424638a3623030392a3f91dbf84fbd1f13
Gary Grotsky <gkgrotsky@aol.com>
2021-11-26 13:45:17 -0500

    v10.10 Comment clean up

--- GPSLCD.h
+++ GPSLCD.h
@@ -28,9 +28,9 @@
 #ifndef GPSLCD_h
 #define GPSLCD_h
 
-//#define VERSION  "10.10"              // Version number
+#define VERSION  "10.10"              // Version number
 //#define Debug                      // Comment out for debug
-#define Serial_Debug               // Comment out for serial monitor display debug
+//#define Serial_Debug               // Comment out for serial monitor display debug
 #define DATA_VALID_OVERRIDE        // Override the data valid flag (comment out to turn off)
                                      // It also feeds fake GPS data to the system for debugging
 
