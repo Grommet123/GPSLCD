c1cde40da672c323a98d418608a0a0e9ed443601
Gary Grotsky <gkgrotsky@aol.com>
2021-11-28 10:55:37 -0500

    v10.14 Slow down serial output for u-blox u-center 2

--- GPSLCD.ino
+++ GPSLCD.ino
@@ -132,6 +132,7 @@ void loop()
     bool b = gps.encode(c = GPSModule.read()); // This feeds the serial NMEA data into the GPS library one char at a time
 #ifdef Debug
     Serial.print(c); // GPS data flow displaying the NMEA data
+    delay (1);
 #endif
     if (b) {
       dataAvailable = true; // Good read
@@ -447,7 +448,7 @@ void loop()
         } // if (dateTimeToggle)
       } // if (now - prevDateTime > TOGGLETIME_INTERVAL)
     } // if ((digitalRead(ALTITUDE_DATE_TIME_SW)))
-  } // if (dataValid)
+  } // if (dataValid || dataValidOverride)
   else {
     // GPS data is not valid, so it must be initializing
     // Turn on red LED (all is not well)
